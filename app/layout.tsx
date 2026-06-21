63a5d9c55bb28bf7bee4fba6564ca52d84f991d3
Yuliia <asee198814@gmail.com>
2026-06-21 08:27:39 +0200

    add hm-09

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
-import AuthProvider from "@/components/AuthProvider/AuthProvider";
+import TanStackProvider from "../components/TanStackProvider/TanStackProvider";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import AuthProvider from "../components/AuthProvider/AuthProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -44,12 +44,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.variable}>
         <TanStackProvider>
-          <Header />
-          <main>
-            {children}
-            {modal}
-          </main>
-          <Footer />
+          <AuthProvider>
+            <Header />
+            <main>
+              {children}
+              {modal}
+            </main>
+            <Footer />
+          </AuthProvider>
         </TanStackProvider>
       </body>
     </html>
